ec03845b79f794c1666cddfddbd72a314b0141a2
Victor Akpan <victxr.a@gmail.com>
2020-02-15 12:59:54 -0600

    Initial Commit

---
+++ FlashCardApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashCardApp
+//
+//  Created by Victor Akpan on 2/15/20.
+//  Copyright © 2020 Victor Akpan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
